79a3ef7141655d41675935d75eca4c54dce74e6c
mathnerdrk <rahul.krishnan@hotmail.com>
2015-03-14 19:18:38 -0600

    Finished SetUp()

--- monopolySim.cpp
+++ monopolySim.cpp
@@ -18,7 +18,8 @@ class Place
 public:
 	string name;
 	void setUp(string x, int placex, Place y);
-	
+	int priceBuy;
+	int priceLand;
 }; 
 Place places[40];
 
@@ -26,11 +27,27 @@ void Place::setUp(string x, int placex, Place y)
 {
 	y.name = x;
 	places[placex] = y;
+	//
 }
+
+void setUp();
 
 int main()
 {
-	//setup
+	setUp();
+	srand(time(NULL));
+
+	//gameStart
+	cout << "Welcome!\n";
+	
+
+	
+	
+	return 0;
+}
+
+void setUp()
+{
 	places[0].setUp("Start", 0, places[0]);
 	places[1].setUp("Mediterranean Avenue", 1, places[1]);
 	places[2].setUp("Community Chest", 2, places[2]);
@@ -42,9 +59,34 @@ int main()
 	places[8].setUp("Vermont Avenue", 8, places[8]);
 	places[9].setUp("Connecticut Avenue", 9, places[9]);
 	places[10].setUp("Just Visiting", 10, places[10]);
-	//Finish other 30 places
-	
-	cout << "Welcome!\n";
-	
-	return 0;
+	places[11].setUp("St. Charles Place", 11, places[11]);
+	places[12].setUp("Electric Company", 12, places[12]);
+	places[13].setUp("States Avenue", 13, places[13]);
+	places[14].setUp("Virginia Avenue", 14, places[14]);
+	places[15].setUp("Pennsylvania Railroad", 15, places[15]);
+	places[16].setUp("St. James Place", 16, places[16]);
+	places[17] = places[2];
+	places[18].setUp("Tennessee Avenue", 18, places[18]);
+	places[19].setUp("New York Avenue", 19, places[19]);
+	places[20].setUp("Free Parking", 20, places[20]);
+	places[21].setUp("Kentucky Avenue", 21, places[21]);
+	places[22] = places[7];
+	places[23].setUp("Indianna Avenue", 23, places[23]);
+	places[24].setUp("Illinois Avenue", 24, places[24]);
+	places[25].setUp("B & O Railroad", 25, places[25]);
+	places[26].setUp("Atlantic Avenue", 26, places[26]);
+	places[27].setUp("Vermont Avenue", 27, places[27]);
+	places[28].setUp("Water Works", 28, places[28]);
+	places[29].setUp("Marvin Gardens", 29, places[29]);
+	places[30].setUp("GO TO JAIL", 30, places[30]);
+	places[31].setUp("Pacific Avenue", 31, places[31]);
+	places[32].setUp("North Carolina Avenue", 32, places[32]);
+	places[33] = places[2];
+	places[34].setUp("Pennsylvania Avenue", 34, places[34]);
+	places[35].setUp("Short Line", 35, places[35]);
+	places[36] = places[7];
+	places[37].setUp("Park Place", 37, places[37]);
+	places[38].setUp("Luxury Tax", 38, places[38]);
+	places[39].setUp("Boardwalk", 39, places[39]);
+	//Finish other places
 }
